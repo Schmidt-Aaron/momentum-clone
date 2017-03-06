26104db1efe41c910f366eb67037934c8e9d3467
John McBride <jpmmcbride@gmail.com>
2017-03-06 12:37:56 -0700

    Initial commit, add files, and add google search
    Hey guys, I've added some intial files as follows
    
    1. Add initial CSS, JS, & HTML files / folders
    2. Add generic license
    3. Create custom google search (this can be customized further, it's
    very basic right now)

---
+++ js/googlesearch.js
@@ -0,0 +1,11 @@
+(function() {
+	var cx = '002428586428036394470:tah0h_n_oyw';
+	var gcse = document.createElement('script');
+
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+	
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+})();
