5fc7c1a1c433b0665f901d897bef4edb2a1453bc
Mike Hickman <pdxmike@users.noreply.github.com>
2017-03-16 18:19:15 -0700

    Remove one line of redundant code

--- js/index.js
+++ js/index.js
@@ -75,7 +75,6 @@ function getPic(){
 	{ quote: "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning", credit: "Rich Cook" }
 	];
 	var random = Math.floor(Math.random() * quotes.length);
-	$("#quote").html("<p>" + quotes[random].quote + "<br>" + " --" + quotes[random].credit);
 
 
 	$.ajax({
